f05631f7a23f00fd4400b5e61041d9f5d8357f6e
Kim Sangyeon <dho113@naver.com>
2020-05-10 10:21:39 +0900

    feature/#9 - service user register 추가

---
+++ src/services/user.service.js
@@ -0,0 +1,33 @@
+import config from '../config';
+
+export const userService = {
+  register,
+};
+
+function register(user) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user),
+  };
+
+  return fetch(config.API.USER_REGISTER_URL, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+
+    if (!response.ok) {
+      if (response.status === 401) {
+        // logout
+        window.location.reload(true);
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
