205837ac95dda653f1b16b10eabc2198dedf3064
Kim Sangyeon <dho113@naver.com>
2020-05-10 22:59:44 +0900

    feature/#8 - App login register Route 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { history } from './helpers';
+
+import { PrivateRoute } from './components';
+import { Main, Login, Register } from './pages';
 
 function App() {
-  return <div className="App">Battle Judging</div>;
+  return (
+    <div className="App">
+      <h1>Battle Judging</h1>
+      <div>
+        <Router history={history}>
+          <Switch>
+            <PrivateRoute exact path="/" component={Main} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Redirect path="*" to="/" />
+          </Switch>
+        </Router>
+      </div>
+    </div>
+  );
 }
 
 export default App;
